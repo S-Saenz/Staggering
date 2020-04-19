6eb909aa5c65055a14605f27ef6596302df942a5
Saenz <sarah.r.saenz@gmail.com>
2020-04-18 22:36:09 -0700

    Update main.js
    added point breakdown and aesthetic explanation from the README

--- src/main.js
+++ src/main.js
@@ -1,3 +1,54 @@
+/*
+Sarah Saenz
+
+Copied from the README:
+
+Credits: Cicada Song by Nature Walker on Youtube - https://www.youtube.com/watch?v=qgGt0F4IJV0 ~royalty free
+
+==================================================================
+
+AESTHETIC EXPLAINATION:
+
+During WW2 the Japanese government restricted magazines, newspapers ads and various other things to using only black and red ink(in addition to the white of the page) due to resource scarsity during the war. I personally love the aesthetic this created and have wanted to work with it for a long time.
+
+In autumn, bugs are really important in Japan. The sound of cicadas is very culturally significant, nobles would sit and listen to those sounds similar to how they would have cherry blossom veiwings in the spring. both of these are still done today.
+
+It is a long standing tradition(granted not a very serious one) in Japan for children to catch stag beetles or rhinoserous beetles in autumn and keep them as pets. (and cock fight with them but whatever)
+
+This game is about a Japanese child catching stag beetles in autumn despite the war. I know I'm so deep.
+
+==================================================================
+
+Point Breakdown:
+
+-Track score that persists across scenes (10)
+
+-Background music -by Nature Walker- (10)
+
+-Parallax scrolling (15)
+
+-Redesign artwork, UI, and sound (50)
+
+85
+
+#FACADE teir
+
+-Create erratic but not glitchy bug-like verticle movement for the beetles(15)
+
+-End screen artwork that is slightly different depending on score (15)
+
+-Hand/rocket is invisible until the catch animation is completed (10)
+
+-Button implementation for menu and end screen(15)
+
+-Music persists to other scenes but the bug fying sound is only played in the Play scene(10)
+
+65
+
+============================TOTAL: 100============================
+===================== with 50pts of padding ======================
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 750,
